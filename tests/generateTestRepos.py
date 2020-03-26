d365b1b1ba515d3fe9586bd03885060ef8d68580
Jamie Bell <jamiebell7777@gmail.com>
2020-03-26 16:33:50 +0000

    script to generate tests complete

--- tests/generateTestRepos.py
+++ tests/generateTestRepos.py
@@ -27,8 +27,44 @@ except:
 
 output = '.'
 if args.output:
-    if not (os.path.exists(args.output) and os.path.isdir()):
+    if not (os.path.exists(args.output) and os.path.isdir(args.output)):
         print("Not a valid output directory: {0}".format(args.outputD))
         sys.exit(1)
     output = args.output
 
+output = os.path.join(output, os.path.basename(args.configFile))
+# Remove '.json' from directory name
+output = output[:-5]
+
+os.mkdir(output)
+
+# Make it an absolute path
+output = os.path.abspath(output)
+
+for pkgName in config:
+    
+    os.chdir(output)
+    os.system('mkdir -p {0}'.format(pkgName))
+    os.chdir(pkgName)
+    os.system('git init')
+
+    for pkgVersion in config[pkgName]:
+        
+        dependencies = {}
+        
+        for dep in config[pkgName][pkgVersion]:
+             dependencies[dep] = {
+                "path": os.path.join(output, dep),
+                "version": config[pkgName][pkgVersion][dep]
+             }
+
+        manifest = {
+            "name": pkgName,
+            "dependencies" : dependencies
+        }
+
+        with open('ipm.json', 'w+') as f:
+            json.dump(manifest, f, sort_keys=True, indent=4)
+
+        os.system('git add . && git commit -m "versionUpgrade"')
+        os.system('git tag v{0}'.format(pkgVersion))
