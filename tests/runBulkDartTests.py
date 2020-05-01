1510096d2be22d6b09817d8320ceac3045206551
Jamie Bell <jamiebell7777@gmail.com>
2020-05-01 23:00:13 +0100

    Untested code for appending results to CSV file

--- tests/runBulkDartTests.py
+++ tests/runBulkDartTests.py
@@ -1,6 +1,6 @@
 #!/home/jamie/Documents/uni/diss/ipm/python-venv-ipm/bin/python3.7
 
-import requests, argparse, sys, json, os, time, subprocess
+import requests, argparse, sys, json, os, time, subprocess, csv
 
 arg_parser = argparse.ArgumentParser(description="Run all generated Dart tests.")
 
@@ -10,11 +10,11 @@ arg_parser.add_argument(
     required=True
     )
 
-# arg_parser.add_argument(
-#     '--csv',
-#     help='The output CSV for results',
-#     required=True
-#     )
+arg_parser.add_argument(
+    '--csv',
+    help='The output CSV for results',
+    required=True
+    )
 
 arg_parser.add_argument(
     '--output', '-o',
@@ -41,7 +41,7 @@ def dart_output_to_json(output):
 
     return json
 
-def run_test_for_pkg_man(ipm=True):
+def run_test_for_pkg_man(pkg, ipm=True):
     start_time = time.time()
     success = True
     try:
@@ -53,30 +53,63 @@ def run_test_for_pkg_man(ipm=True):
         success = False
     run_time = time.time() - start_time
 
-    os.system('mkdir -p {0}'.format(os.path.join(args.output, p_dir)))
+    os.system('mkdir -p {0}'.format(os.path.join(args.output, pkg)))
 
-    with open(os.path.join(args.output, p_dir, ('pub', 'ipm')[ipm] + '.log'), 'w+') as f:
+    with open(os.path.join(args.output, pkg, ('pub', 'ipm')[ipm] + '.log'), 'w+') as f:
         f.write(result)
     
     if success:
-        with open(os.path.join(args.output, p_dir, ('pub', 'ipm')[ipm] + '.json'), 'w+') as f:
+        with open(os.path.join(args.output, pkg, ('pub', 'ipm')[ipm] + '.json'), 'w+') as f:
             if ipm:
                 f.write(result)
             else:
                 json.dump(dart_output_to_json(result), f, sort_keys=True, indent=4)
+
+    return success, run_time, result
 
 pub_path = os.path.join(args.input, 'pub')
 ipm_path = os.path.join(args.input, 'ipm')
 
 pkgs = os.listdir(pub_path)
 
-for p_dir in pkgs:
+for pkg in pkgs:
     os.chdir(pub_path)
-    os.chdir(p_dir)
+    os.chdir(pkg)
     os.system('rm -f pubspec.lock')
-    run_test_for_pkg_man(ipm=False)    
+    
+    pub_success, pub_run_time, pub_result = run_test_for_pkg_man(pkg, ipm=False)    
+
 
     os.chdir(ipm_path)
     os.chdir('pub/')
-    os.chdir(p_dir)
-    run_test_for_pkg_man(ipm=True)    
+    os.chdir(pkg)
+    
+    ipm_success, ipm_run_time, ipm_result = run_test_for_pkg_man(pkg, ipm=True)    
+
+    version = \
+        subprocess \
+        .check_output('echo \'c,.l\' | sudo -S /home/jamie/Documents/uni/diss/ipm/ipm versions', shell=True) \
+        .decode("utf-8") \
+        .splitlines()[-2]
+
+    if pub_success and ipm_success:
+        
+        pub_json = json.loads(pub_result)
+        ipm_json = json.loads(ipm_result)
+
+        # ipm outputs the root package version from version solving too, remove this.
+        del ipm_json['pub/' + pkg]
+        
+        equal_sols = json.dumps(pub_json, sort_keys=True, indent=4) == json.dumps(ipm_json, sort_keys=True, indent=4)
+
+        with open(args.csv, 'a+') as f:
+            writer = csv.writer(f, delimiter=',')
+            writer.writerow([
+                pkg, 
+                version,
+                ('N', 'Y')[pub_success],
+                pub_run_time,
+                ('N', 'Y')[ipm_success],
+                ipm_run_time,
+                ('N', 'Y')[equal_sols]
+            ])
