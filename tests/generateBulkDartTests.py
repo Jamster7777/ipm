3c07397e94f3fdce32c16801b35583c89e777bdb
Jamie Bell <jamiebell7777@gmail.com>
2020-05-02 11:58:10 +0100

    Go to next page to find more tests. Don't regenerate tests for repos already fetched

--- tests/generateBulkDartTests.py
+++ tests/generateBulkDartTests.py
@@ -1,6 +1,6 @@
 #!/home/jamie/Documents/uni/diss/ipm/python-venv-ipm/bin/python3.7
 
-import requests, argparse, sys, json, os, timeit, yaml
+import requests, argparse, sys, json, os, yaml, subprocess
 
 arg_parser = argparse.ArgumentParser(description="Generate several Dart tests based on the most recently published Dart packages.")
 
@@ -24,8 +24,6 @@ arg_parser.add_argument(
 
 args = arg_parser.parse_args(sys.argv[1:])
 
-response_json = requests.get('https://pub.dartlang.org/api/packages?sort=popularity').json()
-
 pub_path = os.path.join(args.output, 'pub')
 ipm_path = os.path.join(args.output, 'ipm')
 
@@ -34,57 +32,71 @@ os.system('mkdir -p {0}'.format(ipm_path))
 
 limit = int(args.limit)
 no_generated = 0
+page_no = 0
 
-for p in response_json['packages']:
+while (no_generated < limit):
 
-    if no_generated >= limit:
-        break
+    page_no = page_no + 1
+    response_json = requests.get('https://pub.dartlang.org/api/packages?page={0}'.format(page_no)).json()
 
-    try:
-        p['latest']['pubspec']['dependencies']['flutter']
-    except:
-        if p['name'] and p['latest']['pubspec'] and p['latest']['pubspec']['homepage'] and ("github.com/" in p['latest']['pubspec']['homepage']):
-            
-            if args.verbose:
-                print(p['name'] + ' has a GitHub url')
+    for p in response_json['packages']:
 
-            check_github = requests.get(p['latest']['pubspec']['homepage'])
+        if no_generated >= limit:
+            break
 
-            if check_github.status_code < 300:
-                
+        p_pub_path = os.path.join(pub_path, p['name'])
+
+        # Skip packages which have already had tests generated for them
+        if not os.path.isdir(p_pub_path):
+
+            # Flutter packages use a different version of pub
+            try:
+                p['latest']['pubspec']['dependencies']['flutter']
+            except:
+                if p['name'] and p['latest']['pubspec'] and p['latest']['pubspec']['homepage'] and ("github.com/" in p['latest']['pubspec']['homepage']):
+
+                    if args.verbose:
+                        print(p['name'] + ' has a GitHub url')
+
+                    # Check the git repo is public to prevent a password prompt
+                    if requests.get(p['latest']['pubspec']['homepage']).status_code < 300:
+                        try:
+                            subprocess \
+                                .check_output('git clone {0} {1}'.format(p['latest']['pubspec']['homepage'], p_pub_path), shell=True)
+                        except subprocess.CalledProcessError:
+                            continue
+
+                        if args.verbose:
+                            print(p['name'] + ' has a cloneable GitHub url')
+
+                        if os.path.isfile(os.path.join(p_pub_path, 'pubspec.yaml')):
+
+                            with open(os.path.join(p_pub_path, 'pubspec.yaml'), 'r') as f:
+                                yamlObj = yaml.load(f)
+
+                            try:
+                                del yamlObj['dev_dependencies']
+                                with open(os.path.join(p_pub_path, 'pubspec.yaml'), 'w+') as f:
+                                    f.write(yaml.dump(yamlObj))
+                            except:
+                                # Ignore
+                                print('No dev_dependencies to delete.')
+
+                            if args.verbose:
+                                print(p['name'] + ' has a pubspec.yaml file in the parent directory')
+                                print('Generating ipm test config for this package...')
+
+                            # There is a pubspec file we can test, so we'll generate an ipm test.
+
+                            os.system('./generateTestsFromDart.py -p {0} -o {1} --bulkPub'.format(p['name'], ipm_path))
+
+                            if args.verbose:
+                                print('Generating test git repos (that do not already exist)')
+
+                            os.system('./generateTestRepos.py -c {0} -o {1} --bulkPub'.format(os.path.join(ipm_path , p['name'] + '.json'), ipm_path))
+                            no_generated = no_generated + 1
+                        else:
+                            os.system('rm -rf {0}'.format(p_pub_path))
+            else:
                 if args.verbose:
-                    print(p['name'] + ' has a PUBLIC GitHub url')
-
-                # Can only test packages with a GitHub url we can clone from, to compare against Dart
-
-                p_pub_path = os.path.join(pub_path, p['name'])
-                os.system('git clone {0} {1}'.format(p['latest']['pubspec']['homepage'], p_pub_path))
-                
-                if os.path.isfile(os.path.join(p_pub_path, 'pubspec.yaml')):
-
-                    with open(os.path.join(p_pub_path, 'pubspec.yaml'), 'r') as f:
-                        yamlObj = yaml.load(f)
-                    
-                    try:
-                        del yamlObj['dev_dependencies']
-                        with open(os.path.join(p_pub_path, 'pubspec.yaml'), 'w+') as f:
-                            f.write(yaml.dump(yamlObj))
-                    except:
-                        # Ignore
-                        print('No dev_dependencies to delete.')
-                
-                    if args.verbose:
-                        print(p['name'] + ' has a pubspec.yaml file in the parent directory')
-                        print('Generating ipm test config for this package...')
-
-                    # There is a pubspec file we can test, so we'll generate an ipm test.
-                    
-                    os.system('./generateTestsFromDart.py -p {0} -o {1} --bulkPub'.format(p['name'], ipm_path))
-
-                    if args.verbose:
-                        print('Generating test git repos (that do not already exist)')
-
-                    os.system('./generateTestRepos.py -c {0} -o {1} --bulkPub'.format(os.path.join(ipm_path , p['name'] + '.json'), ipm_path))
-                    no_generated = no_generated + 1
-                else:
-                    os.system('rm -rf {0}'.format(p_pub_path))
+                    print(p['name'] + ' has already had a test generated for it, skipping.')
