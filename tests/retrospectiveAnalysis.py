34194700c980185c64bf39977d1fb942645173d4
Jamie Bell <jamiebell7777@gmail.com>
2020-05-03 00:27:19 +0100

    Fix retrospective analysis to follow proper order

--- tests/retrospectiveAnalysis.py
+++ tests/retrospectiveAnalysis.py
@@ -1,6 +1,6 @@
 #!/home/jamie/Documents/uni/diss/ipm/python-venv-ipm/bin/python3.7
 
-import requests, argparse, sys, json, os, time, subprocess, csv
+import argparse, sys, json, os, csv
 
 arg_parser = argparse.ArgumentParser(description="Retrospective analysis based on observing the log files of failed tests.")
 
@@ -11,32 +11,42 @@ arg_parser.add_argument(
     )
 
 arg_parser.add_argument(
+    '--originalCSV',
+    help='The original CSV containg the test results.',
+    required=True
+    )
+
+arg_parser.add_argument(
     '--csv',
-    help='The output CSV for results',
+    help='The output CSV for new fields',
     required=True
     )
 
 args = arg_parser.parse_args(sys.argv[1:])
 
-pkgs = os.listdir(args.input)
+pkgs = []
+with open(args.originalCSV) as f:
+    for row in f:
+        pkgs.append(row.split(',')[0])
+
 original_dir = os.getcwd()
 
 for pkg in pkgs:
     os.chdir(args.input)
     os.chdir(pkg)
-    
+
     with open('pub.log', 'r') as f:
-        pub_log = f.read()    
+        pub_log = f.read()
 
     with open('ipm.log', 'r') as f:
         ipm_log = f.read()
-    
+
     if os.path.exists('ipm.json'):
         ipm_success = True
         with open('ipm.json', 'r') as f:
             ipm_json = json.load(f)
     else:
-        ipm_success = False    
+        ipm_success = False
 
     if os.path.exists('pub.json'):
         pub_success = True
@@ -46,12 +56,12 @@ for pkg in pkgs:
         pub_success = False
 
     pub_sdk_error = 'requires SDK version' in pub_log
-    
+
     if ipm_success:
         no_deps = len(ipm_json) == 1
     else:
         no_deps = False
-    
+
     os.chdir(original_dir)
 
     with open(args.csv, 'a+') as f:
